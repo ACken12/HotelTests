dedcb4cfcd42e5c7b88c1ba8f8471cfec30d55cc
corralesK <kimcorralesv@gmail.com>
2025-03-01 12:00:29 -0600

    Unit tests created for room service

--- Application/Services/RoomServiceTests.cs
+++ Application/Services/RoomServiceTests.cs
@@ -24,6 +24,7 @@ namespace HotelTests.Application.Services
         {
             // Arrange
             var room = new Room("101", RoomType.SIMPLE, 100, 2, RoomStatus.DISPONIBLE);
+            _roomRepositoryMock.Setup(repo => repo.GetByRoomNumber("101")).Returns((Room)null);
             _roomRepositoryMock.Setup(repo => repo.Add(It.IsAny<Room>())).Returns(room);
 
             // Act
@@ -42,38 +43,7 @@ namespace HotelTests.Application.Services
             Assert.That(ex.Message, Is.EqualTo("El número de habitación es obligatorio"));
         }
 
-        [Test]
-        public void SearchRooms_ShouldReturnMatchingRooms_WhenCriteriaMatches()
-        {
-            // Arrange
-            var rooms = new List<Room>
-            {
-                new Room("101", RoomType.SIMPLE, 100, 2, RoomStatus.DISPONIBLE),
-                new Room("102", RoomType.DOBLE, 150, 4, RoomStatus.DISPONIBLE)
-            };
-            _roomRepositoryMock.Setup(repo => repo.GetAll()).Returns(rooms);
-
-            // Act
-            var result = _roomService.SearchRooms(RoomType.SIMPLE, null, null);
-
-            // Assert
-            Assert.That(result.Count(), Is.EqualTo(1));
-            Assert.That(result.First().ROOMNUMBER, Is.EqualTo("101"));
-        }
-
-        [Test]
-        public void SearchRooms_ShouldReturnEmptyList_WhenNoMatches()
-        {
-            // Arrange
-            var rooms = new List<Room>();
-            _roomRepositoryMock.Setup(repo => repo.GetAll()).Returns(rooms);
-
-            // Act
-            var result = _roomService.SearchRooms(RoomType.SUITE, 300, 500);
-
-            // Assert
-            Assert.That(result, Is.Empty);
-        }
+        // CheckAvailability Method
 
         [Test]
         public void CheckAvailability_ShouldReturnAvailableRooms_WhenDatesAreValid()
@@ -84,8 +54,7 @@ namespace HotelTests.Application.Services
                 new Room("101", RoomType.SIMPLE, 100, 2, RoomStatus.DISPONIBLE),
                 new Room("102", RoomType.DOBLE, 150, 4, RoomStatus.DISPONIBLE)
             };
-            _roomRepositoryMock.Setup(repo => repo.GetByStatus(RoomStatus.DISPONIBLE)).Returns(rooms);
-            _roomRepositoryMock.Setup(repo => repo.HasReservationsInDateRange(It.IsAny<int>(), It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(false);
+            _roomRepositoryMock.Setup(repo => repo.HasReservationsInDateRange(It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(rooms);
 
             // Act
             var result = _roomService.CheckAvailability(DateTime.Today, DateTime.Today.AddDays(2));
@@ -99,13 +68,136 @@ namespace HotelTests.Application.Services
         {
             // Arrange
             var rooms = new List<Room>();
-            _roomRepositoryMock.Setup(repo => repo.GetByStatus(RoomStatus.DISPONIBLE)).Returns(rooms);
+            _roomRepositoryMock.Setup(repo => repo.HasReservationsInDateRange(It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(rooms);
 
             // Act
             var result = _roomService.CheckAvailability(DateTime.Today, DateTime.Today.AddDays(2));
 
             // Assert
             Assert.That(result, Is.Empty);
+        }
+
+        // SearchRooms Method
+
+        [Test]
+        public void SearchRooms_ShouldReturnRoomsFilteredByType()
+        {
+            // Arrange
+            var rooms = new List<Room>
+            {
+                new Room("101", RoomType.SIMPLE, 100, 2, RoomStatus.DISPONIBLE),
+                new Room("102", RoomType.DOBLE, 150, 4, RoomStatus.DISPONIBLE)
+            };
+            _roomRepositoryMock.Setup(repo => repo.GetByType(RoomType.SIMPLE)).Returns(rooms.Where(r => r.TYPE == RoomType.SIMPLE));
+
+            // Act
+            var result = _roomService.SearchRooms(RoomType.SIMPLE);
+
+            // Assert
+            Assert.That(result.Count(), Is.EqualTo(1));
+            Assert.That(result.First().ROOMNUMBER, Is.EqualTo("101"));
+        }
+
+        [Test]
+        public void SearchRooms_ShouldReturnEmptyList_WhenNoRoomsMatchTypeFilter()
+        {
+            // Arrange
+            var rooms = new List<Room>
+            {
+                new Room("101", RoomType.DOBLE, 100, 2, RoomStatus.DISPONIBLE),
+                new Room("102", RoomType.DOBLE, 150, 4, RoomStatus.DISPONIBLE)
+            };
+            _roomRepositoryMock.Setup(repo => repo.GetByType(RoomType.SIMPLE)).Returns(rooms.Where(r => r.TYPE == RoomType.SIMPLE));
+
+            // Act
+            var result = _roomService.SearchRooms(RoomType.SIMPLE);
+
+            // Assert
+            Assert.That(result.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SearchRooms_ShouldReturnRoomsFilteredByPrice()
+        {
+            // Arrange
+            var rooms = new List<Room>
+            {
+                new Room("101", RoomType.SIMPLE, 100, 2, RoomStatus.DISPONIBLE),
+                new Room("102", RoomType.DOBLE, 150, 4, RoomStatus.DISPONIBLE)
+            };
+            _roomRepositoryMock.Setup(repo => repo.GetByPriceRange(90, 120)).Returns(rooms.Where(r => r.PRICEPERNIGHT >= 90 && r.PRICEPERNIGHT <= 120));
+
+            // Act
+            var result = _roomService.SearchRooms(minPrice: 90, maxPrice: 120);
+
+            // Assert
+            Assert.That(result.Count(), Is.EqualTo(1));
+            Assert.That(result.First().ROOMNUMBER, Is.EqualTo("101"));
+        }
+
+        [Test]
+        public void SearchRooms_ShouldReturnEmptyList_WhenNoRoomsMatchPriceFilter()
+        {
+            // Arrange
+            var rooms = new List<Room>
+            {
+                new Room("101", RoomType.SIMPLE, 100, 2, RoomStatus.DISPONIBLE),
+                new Room("102", RoomType.DOBLE, 150, 4, RoomStatus.DISPONIBLE)
+            };
+            _roomRepositoryMock.Setup(repo => repo.GetByPriceRange(200, 250)).Returns(rooms.Where(r => r.PRICEPERNIGHT >= 200 && r.PRICEPERNIGHT <= 250));
+
+            // Act
+            var result = _roomService.SearchRooms(minPrice: 200, maxPrice: 250);
+
+            // Assert
+            Assert.That(result.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SearchRooms_ShouldReturnRoomsFilteredByDateRange()
+        {
+            // Arrange
+            var rooms = new List<Room>
+            {
+                new Room("101", RoomType.SIMPLE, 100, 2, RoomStatus.DISPONIBLE),
+                new Room("102", RoomType.DOBLE, 150, 4, RoomStatus.DISPONIBLE)
+            };
+            var startDate = DateTime.Today;
+            var endDate = DateTime.Today.AddDays(2);
+            _roomRepositoryMock.Setup(repo => repo.HasReservationsInDateRange(startDate, endDate)).Returns(rooms);
+
+            // Act
+            var result = _roomService.SearchRooms(startDate: startDate, endDate: endDate);
+
+            // Assert
+            Assert.That(result.Count(), Is.EqualTo(2));
+            Assert.That(result.First().ROOMNUMBER, Is.EqualTo("101"));
+            Assert.That(result.Last().ROOMNUMBER, Is.EqualTo("102"));
+        }
+        [Test]
+        public void SearchRooms_ShouldReturnRoomsAvailableToday_WhenNoDateParametersProvided()
+        {
+            // Arrange
+            var rooms = new List<Room>
+            {
+                new Room("101", RoomType.SIMPLE, 100, 2, RoomStatus.DISPONIBLE),
+                new Room("102", RoomType.DOBLE, 150, 4, RoomStatus.DISPONIBLE),
+                new Room("103", RoomType.SIMPLE, 80, 1, RoomStatus.OCUPADO)
+            };
+
+            var today = DateTime.Today;
+            var endDate = today.AddDays(1);
+
+            // Mocking that the method should return rooms that are available today
+            _roomRepositoryMock.Setup(repo => repo.HasReservationsInDateRange(today, endDate)).Returns(rooms.Where(r => r.STATUS == RoomStatus.DISPONIBLE));
+
+            // Act
+            var result = _roomService.SearchRooms();
+
+            // Assert
+            Assert.That(result.Count(), Is.EqualTo(2));
+            Assert.That(result.First().ROOMNUMBER, Is.EqualTo("101"));
+            Assert.That(result.Last().ROOMNUMBER, Is.EqualTo("102"));
         }
     }
 }
